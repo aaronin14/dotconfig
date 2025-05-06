f5c9b6f5739cd08726caaa9d78e68991ac9258ee
Aaron Nguyen <aaron14nguyen@gmail.com>
2025-05-06 00:02:40 -0400

    automated commit

--- dev-env.sh
+++ dev-env.sh
@@ -65,3 +65,6 @@ update_files $DEV_ENV/env/.local $HOME/.local
 
 copy $DEV_ENV/env/.zshrc $HOME/.zshrc
 copy $DEV_ENV/env/.zsh_profile $HOME/.zsh_profile
+copy $DEV_ENV/env/.tmux.conf $HOME/.tmux.conf
+copy $DEV_ENV/env/.tmux-cht-command $HOME/.tmux-cht-command
+copy $DEV_ENV/env/.tmux-cht-languages $HOME/.tmux-cht-languages
